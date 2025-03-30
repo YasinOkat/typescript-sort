cee59d24f83d4f17aaf90366390b9a00d20d9236
Yasin <yasinokat@proton.me>
2025-03-30 16:22:47 +0300

    linked list added

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,9 +1,21 @@
-export class NumbersCollection {
-  constructor(public data: number[]) {}
+import { Sorter } from "./Sorter";
+
+export class NumbersCollection extends Sorter {
+  constructor(public data: number[]) {
+    super();
+  }
+
+  get length(): number {
+    return this.data.length;
+  }
 
   compare(leftIndex: number, rightIndex: number): boolean {
-    if (leftIndex > rightIndex) {
-      data[le];
-    }
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
   }
 }
--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,15 +1,21 @@
-export class Sorter {
-  constructor(public collection: number[]) {}
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
+
+export abstract class Sorter {
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
+  abstract length: number;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
